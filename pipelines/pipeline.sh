7e1d22c22eaa3587b08bcdb5a3fca1db2297a618
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2019-01-08 11:32:07 +1100

    fix path

--- pipelines/pipeline.sh
+++ pipelines/pipeline.sh
@@ -1,5 +1,5 @@
 module load python3/3.6.2
 
-PATH = $PATH:/home/659/aw5153/canu/Linux-amd64/bin/:/home/659/aw5153/flye/bin/:/home/659/aw5153/PBSIM-PacBio-Simulator/src/
+PATH=$PATH:/home/659/aw5153/canu/Linux-amd64/bin/:/home/659/aw5153/flye/bin/:/home/659/aw5153/PBSIM-PacBio-Simulator/src/
 
 python3 assembly_improve.py
