be7136615ff22eb2391c9f2108ce06250dcfc17c
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2019-01-09 16:37:14 +1100

    fix

--- pipelines/pipeline.sh
+++ pipelines/pipeline.sh
@@ -12,7 +12,7 @@ module load java/jdk1.8.0_60
 
 PATH=$PATH:/home/659/aw5153/canu/Linux-amd64/bin/:/home/659/aw5153/flye/bin/:/home/659/aw5153/PBSIM-PacBio-Simulator/src/
 export model_clr_path=/home/659/aw5153/PBSIM-PacBio-Simulator/data/model_qc_clr
-export genonepath=/home/659/aw5153/genome-assembly-graph-analysis/pipelines/input/yeast/YeastAll.fa
+export genonepath=/short/qr59/aw5153/genome-assembly-graph-analysis/pipelines/input/yeast
 
 # export model_clr_path=/media/anuradhawick/data/Tools/PBSIM-PacBio-Simulator/data/model_qc_clr
 # export genonepath=/media/anuradhawick/data/Experiments/Assembly_Graph/pipelines/input/yeast/YeastAll.fa
