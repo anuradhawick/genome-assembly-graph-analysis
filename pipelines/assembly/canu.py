be33854de091af237f9040aa969d051a608f65c1
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2019-01-10 10:21:52 +1100

    fix

--- pipelines/assembly/canu.py
+++ pipelines/assembly/canu.py
@@ -2,7 +2,7 @@ from utils.command_liner import cmd_exit
 
 def run_canu():
     command1 = "[ -e 'output/Canu_yeast' ] && rm -r 'output/Canu_yeast' && mkdir 'output/Canu_yeast'"
-    command2 = "canu -p r1 -pacbio-raw './output/Sampled_Reads_Yeast/all.fastq' -d 'output/Canu_yeast' genomeSize=12m cnsMemory=32 cnsConcurrency=32"
+    command2 = """canu -p r1 -pacbio-raw './output/Sampled_Reads_Yeast/all.fastq' -d 'output/Canu_yeast' genomeSize=12m cnsMemory=32 cnsConcurrency=32 gridEngineThreadsOption='-l ncpus=32'"""
     
     cmd_exit(command1, "creating assembly output path for canu")
     cmd_exit(command2, "running canu")
