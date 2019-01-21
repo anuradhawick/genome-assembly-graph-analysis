05488a191b5a55c7e8d22033594b1b6b4a458b1b
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2019-01-22 09:50:29 +1100

    updated for haplotype assembly

--- pipelines/generate_reads.sh
+++ pipelines/generate_reads.sh
@@ -4,9 +4,6 @@ export genomepath=/short/qr59/aw5153/genome-assembly-graph-analysis/pipelines/in
 # export model_clr_path=/media/anuradhawick/data/Tools/PBSIM-PacBio-Simulator/data/model_qc_clr
 # export genomepath=/media/anuradhawick/data/Experiments/Assembly_Graph/pipelines/input/yeast/all.fa
 
-# gather all reads to one file
-cat input/yeast/chr*.fa > input/yeast/all.fa
-
 # generate reads
 mkdir -p $out_dir/yeast_reads
 cd $out_dir/yeast_reads
--- pipelines/pipeline.sh
+++ pipelines/pipeline.sh
@@ -3,7 +3,7 @@
 #PBS -l ncpus=32
 #PBS -l mem=64GB
 #PBS -l jobfs=1GB
-#PBS -l walltime=5:00:00
+#PBS -l walltime=10:00:00
 #PBS -l wd
 
 module load python3/3.6.2
@@ -13,7 +13,7 @@ module load java/jdk1.8.0_60
 PATH=$PATH:/home/659/aw5153/canu/Linux-amd64/bin/:/home/659/aw5153/flye/bin/:/home/659/aw5153/PBSIM-PacBio-Simulator/src/
 java_path=$(which java)
 
-export out_dir=/short/qr59/aw5153/pipeline_output
+export out_dir=/short/qr59/aw5153/pipeline_output_hap
 # export out_dir=/media/anuradhawick/data/Experiments/Assembly_Graph/pipelines/output
 
 mkdir -p $out_dir
@@ -21,6 +21,6 @@ mkdir -p $out_dir
 export working_path=$(pwd)
 
 
-# sh generate_reads.sh > $out_dir/log1_$PBS_JOBID.log
-# sh canu.sh > $out_dir/log2_$PBS_JOBID.log
+sh generate_reads.sh > $out_dir/log1_$PBS_JOBID.log
+sh canu.sh > $out_dir/log2_$PBS_JOBID.log
 sh flye.sh > $out_dir/log3_$PBS_JOBID.log
