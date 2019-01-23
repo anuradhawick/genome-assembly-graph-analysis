dcfe8ca89f038335640c6c71b12c830622841cfa
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2019-01-24 09:44:06 +1100

    updated pipeline for hap 5%

--- pipelines/pipeline.sh
+++ pipelines/pipeline.sh
@@ -13,7 +13,7 @@ module load java/jdk1.8.0_60
 PATH=$PATH:/home/659/aw5153/canu/Linux-amd64/bin/:/home/659/aw5153/flye/bin/:/home/659/aw5153/PBSIM-PacBio-Simulator/src/
 java_path=$(which java)
 
-export out_dir=/short/qr59/aw5153/pipeline_output_hap
+export out_dir=/short/qr59/aw5153/pipeline_output_hap_5
 # export out_dir=/media/anuradhawick/data/Experiments/Assembly_Graph/pipelines/output
 
 mkdir -p $out_dir
