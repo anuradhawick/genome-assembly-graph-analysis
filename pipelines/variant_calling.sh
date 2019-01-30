2bbfbb18da98fd3ede5bf9821f3fd2cb9fd41523
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2019-01-31 09:47:00 +1100

    variant calling on yeast hap 5%

--- pipelines/variant_calling.sh
+++ pipelines/variant_calling.sh
@@ -26,7 +26,7 @@ mkdir -p $out_dir/vcf_files
 
 echo INFO::Running varian calls:pile up
 
-bcftools mpileup --threads 32 -o $out_dir/vcf_files/study.bcf -Ou -f $contigs_path $out_dir/mappings/mapping.sorted.bam
+bcftools mpileup --threads 32 -o $out_dir/vcf_files/study.bcf -Ou -f $out_dir/contigs.fasta $out_dir/mappings/mapping.sorted.bam
 
 echo INFO::Running varian calls:variant discovery
 
