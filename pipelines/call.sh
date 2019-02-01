62843ba71a105c8e7769df303a0b0c42fd480d2c
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2019-02-01 13:50:07 +1100

    phasing stage

--- pipelines/call.sh
+++ pipelines/call.sh
@@ -7,8 +7,6 @@
 #PBS -l wd
 
 export out_dir=/short/qr59/aw5153/pipeline_output_hap_5_vc
-export graph_path=/short/qr59/aw5153/pipeline_output_hap_5/flye_yeast/assembly_graph.gfa
-export reads_path=/short/qr59/aw5153/pipeline_output_hap_5/yeast_reads/all.fastq
 
 echo INFO::Running varian calls:variant discovery
 bcftools call -c --threads 32 -O b -o $out_dir/vcf_files/initial_call.bcf  $out_dir/vcf_files/study.bcf
--- pipelines/phasing.sh
+++ pipelines/phasing.sh
@@ -7,10 +7,8 @@
 #PBS -l wd
 
 export out_dir=/short/qr59/aw5153/pipeline_output_hap_5_vc
-export graph_path=/short/qr59/aw5153/pipeline_output_hap_5/flye_yeast/assembly_graph.gfa
-export reads_path=/short/qr59/aw5153/pipeline_output_hap_5/yeast_reads/all.fastq
 
 echo INFO::Running phasing calls:preprocess BCF -> VCF
 bcftools view -o $out_dir/vcf_files/initial_call.vcf $out_dir/vcf_files/initial_call.bcf
 echo INFO::Running phasing calls:WhatsHap execution
-whatshap phase -o $out_dir/vcf_files/phased.vcf $out_dir/vcf_files/initial_call.vcf $out_dir/mappings/mapping.sorted.bam
+whatshap phase --ignore-read-groups -r $out_dir/contigs.fasta -o $out_dir/vcf_files/phased.vcf $out_dir/vcf_files/initial_call.vcf $out_dir/mappings/mapping.sorted.bam
