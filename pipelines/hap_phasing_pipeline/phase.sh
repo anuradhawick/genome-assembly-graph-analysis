8cfbffd00a2ebf63c08293fdda7033495cfe6f84
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2019-02-13 13:37:32 +1100

    fixing modules

--- pipelines/hap_phasing_pipeline/phase.sh
+++ pipelines/hap_phasing_pipeline/phase.sh
@@ -1,5 +1,3 @@
-module load bcftools/1.9
-
 echo INFO::Running phasing calls:preprocess BCF -> VCF
 bcftools view -o $out_dir/vcf_files/initial_call.vcf $out_dir/vcf_files/initial_call.bcf
 echo INFO::Running phasing calls:WhatsHap execution
