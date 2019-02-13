357ae99f4edb9631c2b56ae829b2efbbe7d5cb99
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2019-02-13 14:13:26 +1100

    fixing modules

--- pipelines/hap_phasing_pipeline/phase.sh
+++ pipelines/hap_phasing_pipeline/phase.sh
@@ -1,4 +1,4 @@
 echo INFO::Running phasing calls:preprocess BCF -> VCF
 bcftools view -o $out_dir/vcf_files/initial_call.vcf $out_dir/vcf_files/initial_call.bcf
 echo INFO::Running phasing calls:WhatsHap execution
-whatshap phase --ignore-read-groups -r $out_dir/contigs.fasta -o $out_dir/vcf_files/phased.vcf $out_dir/vcf_files/initial_call.vcf $out_dir/mappings/mapping.sorted.bam
+whatshap phase --ignore-read-groups -r $contigs_path -o $out_dir/vcf_files/phased.vcf $out_dir/vcf_files/initial_call.vcf $out_dir/mappings/mapping.sorted.bam
